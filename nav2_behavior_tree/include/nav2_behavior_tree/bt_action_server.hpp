de7b0e17b517115d01fd4b2dc49aacb1f4852f68
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-03-20 10:05:12 +0000

    Make a ROS parameter for it

--- nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server.hpp
@@ -90,11 +90,10 @@ public:
   /**
    * @brief Replace current BT with another one
    * @param bt_xml_filename The file containing the new BT, uses default filename if empty
-   * @param overwrite If true, the new BT will be reloaded even if the filename is the same
    * @return bool true if the resulting BT correspond to the one in bt_xml_filename. false
    * if something went wrong, and previous BT is maintained
    */
-  bool loadBehaviorTree(const std::string & bt_xml_filename = "", bool overwrite = false);
+  bool loadBehaviorTree(const std::string & bt_xml_filename = "");
 
   /**
    * @brief Getter function for BT Blackboard
@@ -232,6 +231,9 @@ protected:
 
   // Default timeout value while waiting for response from a server
   std::chrono::milliseconds default_server_timeout_;
+
+  // should the BT be reloaded even if the same xml filename is requested?
+  bool overwrite_xml_ = false;
 
   // User-provided callbacks
   OnGoalReceivedCallback on_goal_received_callback_;
--- nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/bt_action_server_impl.hpp
@@ -60,6 +60,9 @@ BtActionServer<ActionT>::BtActionServer(
   if (!node->has_parameter("default_server_timeout")) {
     node->declare_parameter("default_server_timeout", 20);
   }
+  if (!node->has_parameter("overwrite_xml")) {
+    node->declare_parameter("overwrite_xml", false);
+  }
 }
 
 template<class ActionT>
@@ -112,6 +115,7 @@ bool BtActionServer<ActionT>::on_configure()
   bt_loop_duration_ = std::chrono::milliseconds(timeout);
   node->get_parameter("default_server_timeout", timeout);
   default_server_timeout_ = std::chrono::milliseconds(timeout);
+  node->get_parameter("overwrite_xml", overwrite_xml_);
 
   // Create the class that registers our custom nodes and executes the BT
   bt_ = std::make_unique<nav2_behavior_tree::BehaviorTreeEngine>(plugin_lib_names_);
@@ -160,13 +164,13 @@ bool BtActionServer<ActionT>::on_cleanup()
 }
 
 template<class ActionT>
-bool BtActionServer<ActionT>::loadBehaviorTree(const std::string & bt_xml_filename, bool overwrite)
+bool BtActionServer<ActionT>::loadBehaviorTree(const std::string & bt_xml_filename)
 {
   // Empty filename is default for backward compatibility
   auto filename = bt_xml_filename.empty() ? default_bt_xml_filename_ : bt_xml_filename;
 
   // Use previous BT if it is the existing one and overwrite flag is not set to true
-  if (!overwrite && current_bt_xml_filename_ == filename) {
+  if (!overwrite_xml_ && current_bt_xml_filename_ == filename) {
     RCLCPP_DEBUG(logger_, "BT will not be reloaded as the given xml is already loaded");
     return true;
   }
